87a3c995d1651419141095bfc06653e8f9eafdd8
KSTA <>
2025-08-07 11:19:54 +0630

    Add model logic for demonstration

--- lib/mvvm/models/product.dart
+++ lib/mvvm/models/product.dart
@@ -23,4 +23,6 @@ class Product {
   factory Product.fromJson(Map<String, dynamic> json) => _$ProductFromJson(json);
 
   Map<String, dynamic> toJson() => _$ProductToJson(this);
+
+  bool get isBudgetFriendly => price <= 100;
 }
